122686ed496d3487fe4fbd1f9cc4f3ad1ab622b7
Dirk Arnez <smalldirkalex@gmail.com>
2026-06-07 20:27:23 +0800

    - update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from typing import Any, List
 import numpy as np
-
+import ifcopenshell
 
 def main():
-  print("hello world")
+  ifc4 = ifcopenshell.schema_by_name("IFC4")
+  
+  
+  for declaration in ifc4.declarations():
+      print(declaration.name()) # 'IfcAbsorbedDoseMeasure', 'IfcAccelerationMeasure', 'IfcActionRequest', ...
+  
+  ifcwall = ifc4.declaration_by_name("IfcWall")
 
 if __name__ == "__main__":
   main()
